63780c40787f28ccbfc3938f770050241ff658b4
Tom Harris <tharris@membersfirst.com>
2025-08-11 09:01:54 -0400

    added code

---
+++ src/ClassicAspIncluder/FileParser.cs
@@ -0,0 +1,75 @@
+﻿namespace ClassicAspIncluder
+{
+    internal class FileParser
+    {
+        public FileParser(string applicationRoot)
+        {
+            _applicationRoot = applicationRoot;
+        }
+
+        public List<string> ParseFile(string? fullPath)
+        {
+            if (string.IsNullOrEmpty(fullPath))
+                return new List<string>();
+
+            if(!File.Exists(fullPath))
+                return new List<string> { $"<% 'Unable to find path: {fullPath}  %>" };
+
+            var fileLines = File.ReadAllLines(fullPath).ToList();
+
+            List<string> completeFile = new List<string>();
+
+            string previousLine = null;
+            foreach (var fileLine in fileLines)
+            {
+                var result = ParseFileLine(fileLine);
+
+                switch(result.Item1)
+                {
+                    case "fileline":
+                        completeFile.Add(result.Item2);
+                        break;
+                    case "virtual":
+                        string virtualFilePath = Path.Combine(_applicationRoot, result.Item2);
+                        completeFile.Add($"<% '''''START {virtualFilePath}''''' %>");
+                        completeFile.AddRange(ParseFile(virtualFilePath));
+                        completeFile.Add($"<% '''''END {virtualFilePath}''''' %>");
+                        break;
+                    case "file":
+                        string fileReferencePath = Path.Combine(Path.GetDirectoryName(fullPath), result.Item2);
+                        completeFile.Add($"<% '''''START {fileReferencePath}''''' %>");
+                        completeFile.AddRange(ParseFile(fileReferencePath));
+                        completeFile.Add($"<% '''''END {fileReferencePath}''''' %>");
+                        break;
+                }
+            }
+
+            return completeFile;
+        }
+
+        private Tuple<string, string> ParseFileLine(string fileLine)
+        {
+            var singleLine = fileLine.Replace(" ", "");
+            if (!singleLine.StartsWith("<!--#include"))
+                return new Tuple<string, string>("fileline", fileLine);
+
+            int firstQuoteIndex = fileLine.IndexOf("\"");
+            int secondQuoteIndex = fileLine.IndexOf("\"", firstQuoteIndex + 1);
+
+            bool isVirtual = fileLine.IndexOf("virtual") != -1;
+            bool isFile = fileLine.IndexOf("file") != -1;
+
+            string includeFilePath = fileLine.Substring(firstQuoteIndex + 1, secondQuoteIndex - firstQuoteIndex - 1);
+
+            if (isVirtual)
+                return new Tuple<string, string>("virtual", includeFilePath.Substring(1));
+
+            if (isFile)
+                return new Tuple<string, string>("file", includeFilePath);
+
+            return new Tuple<string, string>("fileline", fileLine);
+        }
+
+        private string _applicationRoot;
+    }
+}
---
+++ src/ClassicAspIncluder/Program.cs
@@ -0,0 +1,45 @@
+﻿using ClassicAspIncluder;
+
+Console.WriteLine("Enter application root folder path");
+string? rootFolderPath = Console.ReadLine();
+
+if(string.IsNullOrEmpty(rootFolderPath))
+{
+    Console.WriteLine("Invalid root folder path");
+    Console.ReadLine();
+    return;
+}
+
+if(!Directory.Exists(rootFolderPath))
+{
+    Console.WriteLine("Folder does not exist");
+    Console.ReadLine();
+    return;
+}
+
+Console.WriteLine("Enter relative file path");
+string? relativeFilePath = Console.ReadLine();
+
+if(string.IsNullOrEmpty(relativeFilePath))
+{
+    Console.WriteLine("Invalid relative file path");
+    Console.ReadLine();
+    return;
+}
+
+string? fullPath = Path.Combine(rootFolderPath, relativeFilePath);
+Console.WriteLine(fullPath);
+Console.WriteLine();
+
+
+List<string> completeFile = new FileParser(rootFolderPath).ParseFile(fullPath);
+
+
+
+string? fileName = Path.GetFileNameWithoutExtension(relativeFilePath);
+string? fileExt = Path.GetExtension(relativeFilePath);
+string? directoryName = Path.GetDirectoryName(fullPath);
+string? newFileName = $"{fileName}.tmp{fileExt}";
+string newFilePath = Path.Combine(directoryName, newFileName);
+
+File.WriteAllLines(newFilePath, completeFile);
